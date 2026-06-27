6c449248cd1e0c343bbca712cf4461f46664f19f
Taranpal Singh <taranarora1470@gmail.com>
2026-06-27 12:19:19 +0530

    done with booking db schema

--- Backend/model/booking.py
+++ Backend/model/booking.py
@@ -1,8 +1,13 @@
-from pydantic import BaseModel, Field
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
+from database import Base
 
-class Booking(BaseModel):
-    user_id: str
-    booking_id: str
-    event_id: str
-    seats_required: int
-    confirm_booking: bool 
+class BookingsTable(Base):
+    __tablename__ = "bookings"
+
+    booking_id = Column(String, primary_key=True, index=True)
+    user_id = Column(String, ForeignKey("users.user_id"), nullable=False)
+    event_id = Column(String, ForeignKey("events.event_id"), nullable=False)
+    seats_required = Column(Integer, nullable=False)
+    confirm_booking = Column(Boolean, default=False)
+
+
---
+++ Backend/schema/bookings.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+
+
+class BookingRequest(BaseModel):
+    user_id: str
+    event_id: str
+    seats_required: int
+
+
+class BookingResponse(BaseModel):
+    booking_id: str
+    user_id: str
+    event_id: str
+    seats_required: int
+    confirm_booking: bool
+
+    model_config = {
+        "from_attributes": True
+    }
